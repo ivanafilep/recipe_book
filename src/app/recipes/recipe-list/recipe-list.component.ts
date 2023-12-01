56013a27125fe514cc32cdbb97373f2bbb7ca351
ivanafilep <125295042+ivanafilep@users.noreply.github.com>
2023-12-01 15:34:06 +0100

    erasing Output,Input

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,8 +8,7 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit{
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
+  
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService){}
@@ -17,11 +16,5 @@ export class RecipeListComponent implements OnInit{
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
-
-  
-  onRecipeSelected(recipeRef : Recipe){
-    this.recipeWasSelected.emit(recipeRef);
-  }
-
 
 }
