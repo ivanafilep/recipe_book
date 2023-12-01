8d259eaaf6d470eff9f3542d0eedcd78f480c024
ivanafilep <125295042+ivanafilep@users.noreply.github.com>
2023-12-01 14:58:19 +0100

    fixing recipe-list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService{
+    private recipes: Recipe[] = [
+        new Recipe('Gulas', 'Ovo je ukusan gulas', 'https://nova.rs/wp-content/uploads/2021/04/shutterstock_1011502120-725x483.jpg'),
+        new Recipe('Spaghetti Carbonara','Najlepse jelo', 'https://images.24ur.com/media/images/1024x576/Jan2018/f053916a0b_62023484.jpg?v=c812'),
+        new Recipe('Pita sa sirom', 'Domaca pita sa sirom', 'https://podravkaiovariations.blob.core.windows.net/8dcd71e6-6423-11eb-8de0-0242ac12003c/v/f2b1f6a6-64bc-11eb-b6c2-0242ac130010/1024x768-f2b21802-64bc-11eb-a115-0242ac130010.webp'),
+      ];
+
+      //slice koristimo jer ako nesto promenimo u listi recepata, vratice nam novu listu, tj kopiju liste iz ovog servisa.
+      //Tako da od spolja necemo moci da pristupimo direktno listi iz servisa, vec cemo dobiti kopiju
+    getRecipes(){
+        return this.recipes.slice();
+    }
 
 }
