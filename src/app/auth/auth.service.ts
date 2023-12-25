366b5bf8123882ba94ddb355c20455f7d1ab2cf9
ivanafilep <125295042+ivanafilep@users.noreply.github.com>
2023-12-25 16:25:55 +0100

    auto Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData{
 export class AuthService{
 
     user = new BehaviorSubject<UserModel>(null);
+    private tokenExpirationTimer: any;
     
 
     constructor(private http: HttpClient, private router: Router){}
@@ -76,12 +77,25 @@ export class AuthService{
 
         if(loadedUser.token){
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
     }
 
     logOut(){
         this.user.next(null);
-        this.router.navigate(['/auth'])
+        this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;   
+    }
+
+    autoLogout(expirationDuration: number){
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logOut();
+        }, expirationDuration);
     }
 
     private handleAuthentication(
@@ -97,6 +111,7 @@ export class AuthService{
             expirationDate
             );
             this.user.next(user);
+            this.autoLogout(expiresIn * 1000);
             localStorage.setItem('userData', JSON.stringify(user));
     }
     
