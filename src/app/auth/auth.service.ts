011f2df5ee32dda3892dea5a621fb132305900f0
ivanafilep <125295042+ivanafilep@users.noreply.github.com>
2023-12-25 11:57:15 +0100

    authenticatio

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { UserModel } from "./user.model";
 
 export interface AuthResponseData{
     idToken: string;
@@ -15,6 +16,8 @@ export interface AuthResponseData{
 @Injectable({providedIn: 'root'})
 export class AuthService{
 
+    user = new Subject<UserModel>();
+
     constructor(private http: HttpClient){}
 
     signUp(email: string, password: string){
@@ -24,7 +27,14 @@ export class AuthService{
             password: password,
             returnSecureToken : true
         }
-      ).pipe(catchError(this.handleError));
+      ).pipe(catchError(this.handleError), tap(respData => {
+        this.handleAuthentication(
+            respData.email, 
+            respData.localId, 
+            respData.idToken, 
+            +respData.expiresIn)
+      
+      }));
     }
 
     
@@ -35,9 +45,33 @@ export class AuthService{
                 password: password,
                 returnSecureToken : true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), tap(respData => {
+            this.handleAuthentication(
+                respData.email, 
+                respData.localId, 
+                respData.idToken, 
+                +respData.expiresIn)
+          
+          }));
 
     }
+
+    private handleAuthentication(
+        email: string, 
+        userId: string, 
+        token: string, 
+        expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new UserModel(
+            email, 
+            userId,
+            token, 
+            expirationDate
+            );
+            this.user.next(user);
+    }
+    
+
 
     private handleError(errorRes: HttpErrorResponse){
         console.error('Error response:', errorRes); 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Data } from "@angular/router";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 
 })
-export class HeaderComponent{
-    constructor(private dataStorageService: DataStorageService){}
+export class HeaderComponent implements OnInit, OnDestroy{
+
+    private userSub: Subscription;
+    isAuthenticated = false;
+
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
+
+    ngOnInit() {
+       this.userSub = this.authService.user.subscribe(user => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+
+       });
+
+        
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
+    }
 
     onSaveData(){
         this.dataStorageService.storeRecipes();
